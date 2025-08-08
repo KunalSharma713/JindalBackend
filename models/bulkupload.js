fee0f00410e170c65da8bd97ab660678b593fba4
Satish Choudhary <satishchoudhary@startbitsolutions.com>
2025-08-08 17:52:50 +0530

    setup the project

---
+++ models/bulkupload.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bulkuploadSchema = new Schema({
+    type: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String
+    },
+    total_records: {
+        type: Number,
+        default: 0
+    },
+    total_success: {
+        type: Number,
+        default: 0
+    },
+    total_error: {
+        type: Number,
+        default: 0
+    },
+    created_at: {
+        type: Date,
+        default: Date.now,
+    },
+    updated_at: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+module.exports = mongoose.model("bulkupload", bulkuploadSchema);
---
+++ server/app.js
@@ -0,0 +1,88 @@
+// Core modules and third-party packages
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors")
+require("dotenv").config();
+
+
+// Importing route modules
+const userRoutes = require("../routes/AdminuserRoutes"); // Your user routes
+const MobileRoutes = require("../routes/MobileUserRoutes");
+const companyRoutes = require("../routes/AdmincompanyRoutes"); // Your user routes
+const roleRoutes = require("../routes/roleRoutes"); // Your user routes
+const ShipmentRoutes = require("../routes/ShipmentRoutes"); // Your user routes
+const TruckTypeRoutes = require("../routes/TrucksRoutes"); // Your trucks routes
+const BulkUploadRoutes = require("../routes/BulkUploadRoutes")
+const verifyToken = require('../utils/VerifyToken'); // Import the verifyToken middleware
+const dashboardRoutes = require("../routes/Dashboard"); // Your user routes
+
+
+// Initialize the Express app
+const app = express();
+
+// Middleware setup
+app.use(bodyParser.urlencoded({ extended: false })); // Parse URL-encoded payloads
+app.use(bodyParser.json()); // Parse JSON payloads
+
+// Enable CORS for all origins
+app.use(cors({
+    origin: "*"
+}))
+
+const PORT = process.env.PORT || 3000;
+const DB_URL = process.env.DATABASE_URL;
+
+// Set CORS headers for all responses
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+    );
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
+// API route definitions
+app.use("/api/dashboard", dashboardRoutes);
+
+app.use("/api/role", verifyToken, roleRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/company", verifyToken, companyRoutes);
+app.use("/mobile", MobileRoutes)
+app.use("/shipment", verifyToken, ShipmentRoutes)
+app.use("/api/trucks", verifyToken, TruckTypeRoutes)
+app.use("/api/bulkupload", verifyToken, BulkUploadRoutes)
+app.get('/', (req, res) => {
+    res.send('Hello, This is main branch');
+});
+
+
+// Global error handling middleware
+app.use((error, req, res, next) => {
+    console.error(error);
+    res.status(error.statusCode || 500).json({
+        message: error.message || "An unexpected error occurred",
+        data: error.data || null,
+    });
+});
+
+// Database connection
+mongoose
+    .connect(DB_URL)
+    .then(() => {
+        console.log('MongoDB connected');
+    })
+    .catch((err) => {
+        console.error("❌ Failed to connect to MongoDB", err);
+        process.exit(1);
+    });
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
+
+// Export a handler for Vercel
+module.exports = app;
