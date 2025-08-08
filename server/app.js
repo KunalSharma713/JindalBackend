a652f72463ae889cadef7a9a42cf5f0bd7916e24
Satish Choudhary <satishchoudhary@startbitsolutions.com>
2025-08-08 18:04:44 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -25,10 +25,13 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false })); // Parse URL-encoded payloads
 app.use(bodyParser.json()); // Parse JSON payloads
 
-// Enable CORS for all origins
 app.use(cors({
-    origin: "*"
-}))
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
+app.options('*', cors());
 
 const PORT = process.env.PORT || 3000;
 const DB_URL = process.env.DATABASE_URL;
